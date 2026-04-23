d413d8c9659c08b1837d21f92c5422c00f7ef492
shribinwade <shribinwade.9103@gmail.com>
2026-04-24 03:11:53 +0530

    added created kafkaconfig class kafka listing bug resolved added @Service annotation on AuthServiceConsumer

--- src/main/java/userservice/config/UserConfig.java
+++ src/main/java/userservice/config/UserConfig.java
@@ -3,6 +3,7 @@ package userservice.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
 public class UserConfig {
--- src/main/java/userservice/consumer/AuthServiceConsumer.java
+++ src/main/java/userservice/consumer/AuthServiceConsumer.java
@@ -1,9 +1,11 @@
 package userservice.consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import userservice.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 
+@Service
 public class AuthServiceConsumer {
 
     private UserRepository userRepository;
@@ -16,7 +18,7 @@ public class AuthServiceConsumer {
     @KafkaListener(topics = "${spring.kafka.topic-json.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void lister(Object eventData){
          try{
-
+             System.out.println("Received: " + eventData);
          }catch (Exception ex){
              ex.printStackTrace();
          }
